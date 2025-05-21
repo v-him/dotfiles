7554eecca674c4bd392280492d27ffdb40dba80a
Hernan Ibarra Mejia <hernanibarramejia@tutamail.com>
2025-05-21 13:51:45 +0100

    Nvim keybindings: moving over wrapped lines

---
+++ nvim/ftplugin/text.lua
@@ -0,0 +1,4 @@
+local set = vim.keymap.set
+
+set({ 'n', 'v' }, 'j', 'gj', { desc = 'Move down quickly over long lines' })
+set({ 'n', 'v' }, 'k', 'gk', { desc = 'Move up quickly over long lines' })
