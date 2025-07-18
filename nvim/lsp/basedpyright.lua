47dfad62c4091c64c21eb7ea4ff4ebef2ce4abd7
Hernan Ibarra Mejia <hernanibarramejia@tutamail.com>
2025-07-18 19:02:15 +0100

    Add basedpyright to lsps

---
+++ nvim/lsp/basedpyright.lua
@@ -0,0 +1,14 @@
+return {
+  cmd = { "basedpyright-langserver", "--stdio" },
+  filetypes = { 'python' },
+  root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", "pyrightconfig.json", ".git" },
+  settings = {
+    basedpyright = {
+      analysis = {
+        autoSearchPaths = true,
+        diagnosticMode = "openFilesOnly",
+        useLibraryCodeForTypes = true
+      }
+    }
+  }
+}
